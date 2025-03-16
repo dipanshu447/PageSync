5d74def0253288cd6c0eaededd281ceabb6a1a95
dipanshuu <dipanshusahu447@gmail.com>
2025-03-16 20:25:39 +0530

    Fixes and added favi icon

--- script.js
+++ script.js
@@ -6,11 +6,15 @@ var submitBtn = document.getElementById("submit");
 let calculateCurrentPag = (oldbook_currentPage, oldbook_TotalPage, newBook_TotalPage) => {
     return Math.round((oldbook_currentPage/oldbook_TotalPage) * newBook_TotalPage);
 }
-
+var resultEle = document.getElementById("result");
 submitBtn.addEventListener("click",() => {
     let result = calculateCurrentPag(oldBook_CurrentPage.value,oldBook_TotalPage.value,newBook_Total.value);
+    if (isNaN(result)) {
+        resultEle.innerText = "New Book's Current Page: " + 0 + " pages";
+    }else {
 
-    document.getElementById("result").innerText = "New Book's Current Page: " + result + " pages";
+        resultEle.innerText = "New Book's Current Page: " + result + " pages";
+    }
 })
 
 
