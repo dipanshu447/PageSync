0bc55b217da2c9fc08bc6472485307588a064686
dipanshuu <dipanshusahu447@gmail.com>
2025-03-16 20:02:15 +0530

    added js logic

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var oldBook_CurrentPage = document.getElementById("old_current-page");
+var oldBook_TotalPage = document.getElementById("old_total-page");
+var newBook_Total = document.getElementById("newBook");
+var submitBtn = document.getElementById("submit");
+
 let calculateCurrentPag = (oldbook_currentPage, oldbook_TotalPage, newBook_TotalPage) => {
     return Math.round((oldbook_currentPage/oldbook_TotalPage) * newBook_TotalPage);
 }
+
+submitBtn.addEventListener("click",() => {
+    let result = calculateCurrentPag(oldBook_CurrentPage.value,oldBook_TotalPage.value,newBook_Total.value);
+
+    document.getElementById("result").innerText = "New Book's Current Page: " + result + " pages";
+})
+
+
+
